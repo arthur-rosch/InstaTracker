5dd3eb0a491ba6fcf3b34e78b258647ee550ff73
Arthur Rosch <65727437+arthur-rosch@users.noreply.github.com>
2025-07-24 18:55:17 -0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'InstaCheck',
+  description: 'InstaCheck',
+  generator: 'InstaCheck',
 }
 
 export default function RootLayout({
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/public/images/spy-icon.png" type="image/png" />
+      </head>
       <body>{children}</body>
     </html>
   )
